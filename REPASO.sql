70fd86e17e48d386ac0a19903e7cd774dedb1cb9
Mig <villegasmazomiguelangel@gmail.com>
2026-02-20 19:20:51 -0500

    Agregar trigger

--- REPASO.sql
+++ REPASO.sql
@@ -7,7 +7,8 @@ SELECT * FROM Clientes
 SELECT * FROM Facturas --Identificacion (Clientes)
 SELECT * FROM DetalleFactura --Id Factura, Id Productos
 
--- 1. INNER JOIN ----------------------------------------------------------------------------
+-- 1) JOINS =================================================================================
+-- 1. INNER JOIN 
 SELECT C.IdCategoria, C.NombreCat, P.NombrePro, P.Precio
 FROM Categorias AS C INNER JOIN Productos AS P ON C.IdCategoria = P.IdCategoria
 
@@ -37,7 +38,7 @@ FROM Categorias CROSS JOIN Productos
 SELECT *
 FROM Categorias AS C FULL JOIN Productos AS P ON C.IdCategoria = P.IdProducto
 
--- VISTAS ---------------------------------------------------------------------------------------------
+-- 2) VISTAS =================================================================================
 /*  Crear una vista en donde muestre a todas las categorias y la posible relación y 
 	detalle producto y crear una nueva columna llamada "Stock"*/
 
@@ -54,8 +55,8 @@ SELECT * FROM v_DetalleProducto
 -- DROP VIEW
 DROP VIEW v_DetalleProducto
 
--- PROCEDIMIENTOS ------------------------------------------------------------------------------------
--- 1. CREATE ==============================
+-- 3) PROCEDIMIENTOS =========================================================================
+-- 1. CREATE -----------------------
 CREATE PROCEDURE sp_InsertarCategoria
 	@NombreCat VARCHAR(40),
 	@Descripcion VARCHAR(40)
@@ -71,7 +72,7 @@ DROP PROCEDURE sp_InsertarCategoria
 -- MOSTRAR LA CATEGORIA 
 SELECT * FROM Categorias
 
--- 2. READ =============================================
+-- 2. READ ------------------------------------------
 CREATE PROCEDURE sp_buscarCompraCliente
 	@identificacion INT
 	AS 
@@ -86,7 +87,7 @@ CREATE PROCEDURE sp_buscarCompraCliente
 EXEC sp_buscarCompraCliente 34
 DROP PROCEDURE sp_buscarCompraCliente
 
--- 3. UPDATE ===============================================
+-- 3. UPDATE -----------------------------------------
 CREATE PROCEDURE sp_actualizarDatosGeneral
 	@IdProducto INT,
 	@IdCategoria INT, 
@@ -102,7 +103,7 @@ EXEC sp_actualizarDatosGeneral 3, 11, 'Play station 5', 26
 SELECT * FROM Productos WHERE IdProducto = 3
 DROP PROCEDURE sp_actualizarDatosGeneral
 
--- 4. ELIMINAR ===============================================
+-- 4. ELIMINAR ----------------------------------------
 CREATE PROCEDURE sp_eliminarCategoria
 	@IdCategoria INT
 	AS 
@@ -112,7 +113,7 @@ EXEC sp_eliminarCategoria 12
 -- VER 
 SELECT * FROM Categorias
 
--- CASE ------------------------------------------------------------------
+-- 4) CASE =============================================================================
 -- Sin parámetro
 CREATE VIEW v_verificarStock AS
 	SELECT NombrePro, cantidad,
@@ -140,3 +141,52 @@ AS
 	FROM Productos
 -- EJECUTAR
 SELECT * FROM v_verEstado
+SELECT * FROM Productos
+SELECT * FROM DetalleFactura
+
+-- 5) TRIGGERS =============================================================================
+
+-- Actualizar el stock teniendo en cuenta la cantidad comprada
+CREATE TRIGGER trg_ActualizarStock
+ON DetalleFactura
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    -- VALIDAR STOCK
+    IF EXISTS (
+        SELECT 1
+        FROM inserted I
+        INNER JOIN Productos P 
+            ON I.IdProducto = P.IdProducto
+        WHERE I.Cantidad > P.CantidadStock
+    )
+    BEGIN
+        RAISERROR('No hay stock suficiente',16,1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END
+
+
+    -- INSERT (RESTAR STOCK) 
+    UPDATE P SET P.CantidadStock = P.CantidadStock - I.Cantidad
+    FROM Productos AS  P INNER JOIN inserted AS I ON P.IdProducto = I.IdProducto;
+
+	-- UPDATE (Actualizar el stock teniendo en cuenta la nueva cantidad)
+    UPDATE P SET P.CantidadStock = P.CantidadStock + D.Cantidad - I.Cantidad
+    FROM Productos AS P
+    INNER JOIN deleted AS D ON P.IdProducto = D.IdProducto
+    INNER JOIN inserted AS I  ON I.IdProducto = D.IdProducto;
+
+    -- DELETE (Sumar al stock la cantidad previamente comprada)
+    UPDATE P SET P.CantidadStock = P.CantidadStock + D.Cantidad
+    FROM Productos AS P INNER JOIN deleted AS D ON P.IdProducto = D.IdProducto;
+END
+
+-- Insertar datos
+INSERT INTO DetalleFactura VALUES (4, 20, 3, 5)
+
+-- Actualizar
+UPDATE DetalleFactura SET Cantidad = 10 WHERE IdDetalle = 7000
+
+-- Eliminar 
+DELETE DetalleFactura WHERE IdDetalle = 7000
